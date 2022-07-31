9337eb5fe94b8b43cd2548f8d4e5e5728e64681f
Mubarak <ahmed.mubarak@linkdev.com>
2022-07-31 10:01:08 +0200

    Add project files.

---
+++ Controllers/SecretsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenShiftSecrets.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SecretsController : Controller
+    {
+        [HttpGet(Name = "Secret")]
+        public string Get()
+        {
+            return "Hi";
+        }
+
+        [HttpGet(Name = "Secret")]
+        public string Dir(string folder = @"ConfigMap/Secret")
+        {
+            try
+            {
+                DirectoryInfo d = new DirectoryInfo(folder);
+
+                FileInfo[] Files = d.GetFiles("*.txt");
+                string str = "";
+
+                foreach (FileInfo file in Files)
+                {
+                    str = str + ", " + file.Name;
+                }
+                return str;
+            }
+            catch (Exception ex)
+            {
+                return ex.InnerException.ToString();
+           
+            }
+
+        }
+
+
+    }
+}
