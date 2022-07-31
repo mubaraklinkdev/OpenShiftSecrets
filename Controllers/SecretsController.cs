032c70f7d12e5d2bf440793adf63d5dc00b9d7e6
Mubarak <ahmed.mubarak@linkdev.com>
2022-07-31 10:15:53 +0200

    Add swagger

--- Controllers/SecretsController.cs
+++ Controllers/SecretsController.cs
@@ -4,23 +4,23 @@ using Microsoft.AspNetCore.Mvc;
 namespace OpenShiftSecrets.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("Secrets")]
     public class SecretsController : Controller
     {
-        [HttpGet(Name = "Secret")]
+        [HttpGet("Secret")]
         public string Get()
         {
             return "Hi";
         }
 
-        [HttpGet(Name = "Secret")]
+        [HttpGet("Dir")]
         public string Dir(string folder = @"ConfigMap/Secret")
         {
             try
             {
                 DirectoryInfo d = new DirectoryInfo(folder);
 
-                FileInfo[] Files = d.GetFiles("*.txt");
+                FileInfo[] Files = d.GetFiles();
                 string str = "";
 
                 foreach (FileInfo file in Files)
@@ -31,7 +31,7 @@ namespace OpenShiftSecrets.Controllers
             }
             catch (Exception ex)
             {
-                return ex.InnerException.ToString();
+                return ex.ToString();
            
             }
 
